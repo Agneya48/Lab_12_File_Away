f7c6682c4905911c38b7d3549c66904b02cb9897
Agneya48 <josh.t.hampton@gmail.com>
2024-12-02 08:08:25 -0500

    Working Version of FileInspector done

--- src/FileInspector.java
+++ src/FileInspector.java
@@ -1,5 +1,76 @@
-public class Main {
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class FileInspector {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+
+        try {
+            // uses a fixed known path:
+            //  Path file = Paths.get("c:\\My Documents\\data.txt");
+
+            // use the toolkit to get the current working directory of the IDE
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            // Typically, we want the user to pick the file so we use a file chooser
+            // kind of ugly code to make the chooser work with NIO.
+            chooser.setCurrentDirectory(workingDirectory);
+            // Using the chooser adds some complexity to the code.
+            // we have to code the complete program within the conditional return of
+            // the filechooser because the user can close it without picking a file
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                // Typical java pattern of inherited classes
+                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                int line = 0;
+                int words = 0;
+                int characters = 0;
+                String dataElements[];
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    characters = characters + rec.length();
+                    dataElements = rec.split(" ");
+                    words = words + dataElements.length;
+                    System.out.println(words);
+                    line++;
+                    // echo to screen
+                    System.out.printf("\nLine %4d %-60s ", line, rec);
+                }
+                reader.close(); // must close the file to seal it and flush buffer
+                System.out.println("\n\nData file read!");
+                System.out.println("Summary");
+                System.out.println("Lines: " + line);
+                System.out.println("Words: " + words);
+                System.out.println("Characters: " + characters);
+
+
+            } else  // User closed the chooser without selecting a file
+            {
+                System.out.println("No file selected!!! ... exiting.\nRun the program again and select a file.");
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
